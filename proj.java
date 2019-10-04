112865f9a457ea5f012c29ccfe0fd43e4a527c1e
SIUCMalika <54688270+SIUCMalika@users.noreply.github.com>
2019-10-03 19:09:46 -0500

    Find area

---
+++ proj.java
@@ -0,0 +1,11 @@
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=15;
+		double area= a*b;
+		System.out.println(area);
+	}
+
+}
